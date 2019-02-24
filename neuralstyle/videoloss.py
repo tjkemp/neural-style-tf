aa70bbf961537e19029632af72506daea0a01ff1
Tero Kemppi <tero.kemppi@gmail.com>
2019-02-24 15:43:52 +0200

    moved video related function with other similar

--- neuralstyle/videoloss.py
+++ neuralstyle/videoloss.py
@@ -1,3 +1,19 @@
+import numpy as np
+import tensorflow as tf
+
+'''
+  'artistic style transfer for videos' loss functions
+'''
+
+
+def temporal_loss(x, w, c):
+    c = c[np.newaxis, :, :, :]
+    D = float(x.size)
+    loss = (1. / D) * tf.reduce_sum(c * tf.nn.l2_loss(x - w))
+    loss = tf.cast(loss, tf.float32)
+    return loss
+
+
 def get_longterm_weights(i, j):
     c_sum = 0.
     for k in range(args.prev_frame_indices):
